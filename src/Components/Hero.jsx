24875419739b485fd89c4fd27a828b3364ff6927
Ismael <ismael.lajud65@gmail.com>
2024-09-20 23:03:27 +0200

    first commit

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,51 @@
+import { useEffect } from 'react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+import '../Styles/StylesHero.css'
+const Hero = () => {
+
+    useEffect(() => {
+        Aos.init({
+          duration: 1000
+        })
+      }, [])
+  return (
+    <main className='hero-main'>
+    <header className='header-hero'>
+   
+        <div className='logo-container'>
+<img src="Images/LogoStartup.png" className='logo-hero' />
+        </div>
+        <nav className='nav-hero' >
+        <div className='services-container'>
+                <ul className='nav-list'>
+                    <li className='list-item'>Marketing</li>
+                    <li className='list-item'>Branding</li>
+                    <li className='list-item'>Web</li>
+                    <li className='list-item'>Proyectos</li>
+                    <li className='list-item'>Contacto</li>
+                </ul>
+        </div>
+    </nav>
+   
+    </header>
+    <section className='section-hero'>
+        <h1 className='hero-slogan' data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom">Si no eres diferente,<br/>eres indiferente</h1>
+<div className='hero-description'>
+<button className='hero-button'>
+    QUIERO VER MÁS
+     </button>
+     <p className='p-hero'>
+        La Agencia de Marketing Digital,<br/>que se adapta a tus necesidades.
+     </p>
+     </div>
+     </section>
+ 
+    </main>
+    
+  )
+}
+
+export default Hero
