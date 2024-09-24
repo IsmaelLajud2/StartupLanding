e66f270017a7a96c60c7a7df280287a154b04f1d
Ismael <ismael.lajud65@gmail.com>
2024-09-24 21:31:17 +0200

    actualizando

--- src/Components/MarketingSection.jsx
+++ src/Components/MarketingSection.jsx
@@ -1,31 +1,46 @@
 import '../Styles/MarketingStyles.css'
+import 'aos/dist/aos.css'
 const MarketingSection = () => {
     return (
         <section className="section-marketing">
             <div className='div-left'>
-                <p className='p-title'>Plan de marketin Avanzado</p>
-                <h1 className='h1-marketing'>¿De que te sirve la visibilidad si lo que vendes no convence?</h1>
+                <p className='p-title' data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top-bottom"  >PLAN DE MARKETING DIGITAL AVANZADO</p>
+                <h1 className='h1-marketing' data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top-bottom">¿De que te sirve la visibilidad si lo que vendes no convence?</h1>
 
-                <p className='p-horizontes'>Ya no vale todo ,tu compentencia es demasiado buena y el público objetivo al que te presentas
+                <p className='p-horizontes' data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top-bottom">Ya no vale todo ,tu compentencia es demasiado buena y el público objetivo al que te presentas
                     cada vez mas exigante.
                 </p>
-                <p className='p-horizontes'>
+                <p className='p-horizontes' data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top-bottom">
                     <strong>Expande tus horizontes:</strong> Digitaliza y activa los mecanismos para que te compren. El Programa de Marketing Digital Definitivo para impulsar tu Marca diseñado específicamente para Emprendedores y PYMES.
                     A través de diferentes fases estratégicas, te ayudaremos a destacar entre tu competencia y conquistar a tu público objetivo.
                 </p>
 
                 <div>
-                    <button className='button-contacto'>Contáctanos</button>
+                    <button className='button-contacto' data-aos="fade-up" data-aos-duration="700" data-aos-anchor-placement="top-bottom">Contáctanos</button>
                 </div>
             </div>
             <div className='div-right'>
-                <div className='imagen-container'>
-                    <img className='imagen-computer' src='/Images/Cards/CardFondo2.png'></img>
+                <div className='imagen-container' >
 
+
+                    <img className='imagen-computer' alt='laptop' src='Images/LaptopHd.png' data-aos-anchor-placement="top-bottom" data-aos="fade-up" data-aos-duration="1000" ></img>
+                    <p className='p-eslogan' data-aos-anchor-placement="top-bottom" data-aos="fade-up" data-aos-duration="1000">Transforma tu negocio con estrategias de Marketing Digital efectivas.</p>
+                </div>
+                <div className='slogan-marketing' data-aos-anchor-placement="top-bottom" data-aos="fade-up" data-aos-duration="1000">
+                    <ul className='list-marketing'>
+                        <li className='item-list'>- Diseño de <b>Webs a medida</b></li>
+                        <li className='item-list'>- <b>Automatizacion</b> de procesos</li>
+                        <li className='item-list'>-<b> Planificación</b> de Ventas</li>
+                        <li className='item-list'>- Estratagia de <b>Marketing Digital</b></li>
+                        <li className='item-list'>-<b> Analisas </b>de Procesos</li>
+                        <li className='item-list'>-<b> Branding </b>y comunicacion</li>
+                        <li className='item-list'>- Creacion de <b>Campañas</b></li>
+
+                    </ul>
                 </div>
             </div>
 
-        </section>
+        </section >
     )
 }
 
--- src/Components/SeccionCard/SectionProyects.jsx
+++ src/Components/SeccionCard/SectionProyects.jsx
@@ -2,19 +2,20 @@ import '../SeccionCard/CardSectionStyles.css'
 import { ScrollParallax } from "react-just-parallax";
 import dataCard from '../SeccionCard/Cards.json'
 import CardProyect from './CardProyect';
+import 'aos/dist/aos.css'
 import EyeSvg from '../SVG/EyeSvg';
 const SectionProyects = () => {
 
 
- 
+
   return (
     <section className='section-container'>
       <div className='text-container'>
         <h1 className='card-title'>
           Es Hora de
           <div className="parallax-wrapper">
-            <ScrollParallax 
-              strength={-0.4} 
+            <ScrollParallax
+              strength={-0.4}
               lerpEase={0.7}
             >
               <span className='paralax-text'>
@@ -24,19 +25,23 @@ const SectionProyects = () => {
           </div>
         </h1>
       </div>
-      <div className='card-container'>
+      <div className='card-container' data-aos="fade-right"
+        data-aos-offset="100"
+        data-aos-easing="ease-in-sine">
         {
-          dataCard.proyectos.map((servicio,index) =>{
+          dataCard.proyectos.map((servicio, index) => {
 
-            
-            return(
 
-             <CardProyect key={index} imagen={servicio.imagen} descripcion={servicio.descripcion} titulo={servicio.titulo} ></CardProyect>
-          )})
+            return (
+
+              <CardProyect key={index} imagen={servicio.imagen} descripcion={servicio.descripcion} titulo={servicio.titulo} ></CardProyect>
+            )
+          })
 
         }
       </div>
-      <section className='section-icon'>
+      <section className='section-icon' data-aos="fade-up"
+        data-aos-anchor-placement="center-bottom" >
         <span className='span-icon' ><EyeSvg></EyeSvg></span> <p className='p-icon'> Quiero ver más servicios</p>
       </section>
     </section>
