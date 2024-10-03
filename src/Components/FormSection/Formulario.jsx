8d88af6482c72e100ee3e3445adf773aa724fc6a
Ismael <ismael.lajud65@gmail.com>
2024-10-03 21:16:17 +0200

    actualizando

--- src/Components/FormSection/Formulario.jsx
+++ src/Components/FormSection/Formulario.jsx
@@ -6,7 +6,7 @@ const Formulario = () => {
     return (
         <section className='container-form'>
             <div className='form-text' data-aos="fade-down"
-            >
+                data-aos-offset="100">
                 <p className='form-p'>IMPULSAMOS TU NEGOCIO </p>
                 <h1 className='h1-form'>INNOVEMOS JUNTOS</h1>
                 <p className='form-article'>Forma parte de nuestro equipo y comienza a transformar el futuro de tu negocio. Juntos, lograremos que tu marca no solo destaque, sino que sea memorable. Si deseas más información sobre Marketing Digital, Diseño Web, Branding o Publicidad, no dudes en contactarnos. Estamos aquí para ayudarte y resolver todas tus inquietudes</p>
